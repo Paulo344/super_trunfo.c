20617be4867c13f40a632f5fd63e527787fbbb45
Paulo344 <124565087+Paulo344@users.noreply.github.com>
2025-03-13 22:16:14 -0300

    Add files via upload
    update do super

--- super_triumf.c
+++ super_triumf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    char estado[50];
+    char estado[10];
     char NomedaCidade[50];
     int CodigoDaCarta;
     int população ;
@@ -9,7 +9,8 @@ int main(){
     float PIB;
     int pontos;
     int carta;
-
+    float DensidadePopulacional;
+    float PIBperCapita;
     //DEFINICAO DAS VARIAVEIS//
 
  printf("Digite o estado:\n");
@@ -33,13 +34,18 @@ int main(){
  printf("Qual o numero da sua carta\n");
  scanf("%d",carta);
 
+ PIBperCapita =(float)(PIB/população);
+ DensidadePopulacional=(float)(população/area);
+
      //PEDIDO E LEITURA DOS DADOS//
 
  printf("Sua carta e :%d\n",carta);
  printf("O numero de pontos turisticos do seu estado e :%d\n",pontos);
  printf("O PIB do seu estado e:%f\n",PIB);
+ printf("O PIB por pessoa e de:%f\n",PIBperCapita);
  printf("A area do seu estado e:%f\n",area);
  printf("Sua população e de :%d\n",população);
+ printf("A densidade populacional da sua cidade e de: %f\n",DensidadePopulacional);
  printf("Seu codigo e :%d\n",CodigoDaCarta);
  printf("Seu estado e :%s\n",estado);
  return 0;
